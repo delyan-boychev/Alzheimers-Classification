5b83a5139ce9a64d7f82b2a35b3be7f47ff532cd
Delyan Boychev <delyan.boychev05@gmail.com>
2022-05-01 16:46:02 +0300

    Generate dataset working

---
+++ data/generate_dataset.py
@@ -0,0 +1,46 @@
+import csv
+import os
+import enumClasses
+import random
+import zipfile
+import shutil
+
+dataset_archive = "./raw_dataset.zip"
+folder_extracted = "./Dataset"
+folder_images = "./Images"
+
+with zipfile.ZipFile(dataset_archive, 'r') as zip_ref:
+    zip_ref.extractall("./")
+
+directories = os.listdir(folder_extracted)
+l = []
+
+f = open('./train.csv', 'w')
+f2 = open('./test.csv', 'w')
+w = csv.writer(f)
+w2 = csv.writer(f2)
+for dir in directories:
+    for file in os.listdir(os.path.join(folder_extracted, dir)):
+        l.append([enumClasses.Classes[dir].value, file])
+random.shuffle(l)
+train = l[:int(len(l)*0.8)]
+test = l[int(len(l)*0.8):]
+for tr in train:
+    w.writerow(tr)
+for ts in test:
+    w2.writerow(ts)
+f.close()
+f2.close()
+if not os.path.exists(folder_images):
+    os.mkdir(folder_images)
+for root, dirs, files in os.walk(folder_extracted, topdown=False):
+    for name in files:
+        if os.path.isfile(os.path.join(root, name)):
+            os.rename(os.path.join(root, name),
+                      os.path.join(folder_images, name))
+    for name in dirs:
+        if os.path.isfile(os.path.join(root, name)):
+            os.rename(os.path.join(root, name),
+                      os.path.join(folder_images, name))
+shutil.rmtree(folder_extracted)
+print("Dataset generated!")
